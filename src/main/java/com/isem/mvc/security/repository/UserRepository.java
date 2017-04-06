449ba6e069d95d89a3dea36a4fa107223c74855f
radni4321 <radni4321@yahoo.com>
2017-04-06 10:09:17 +0200

    security

---
+++ src/main/java/com/isem/mvc/security/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.isem.mvc.security.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import com.isem.mvc.model.security.User;
+
+/**
+ * Created by stephan on 20.03.16.
+ */
+@RepositoryRestResource(exported = false)
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
