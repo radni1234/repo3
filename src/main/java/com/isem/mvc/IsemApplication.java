19c5f5d5b1e8fa1385069e038059ef8f301c0df5
radni1234@yahoo.com <radni1234@yahoo.com>
2017-10-11 22:03:36 +0200

    upload

--- src/main/java/com/isem/mvc/IsemApplication.java
+++ src/main/java/com/isem/mvc/IsemApplication.java
@@ -1,12 +1,19 @@
 package com.isem.mvc;
 
+import javax.annotation.Resource;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.isem.mvc.service.StorageService;
+
 @SpringBootApplication
 public class IsemApplication {
-
+	@Resource	
+	StorageService storageService;
+	
 	public static void main(String[] args) {
-		SpringApplication.run(IsemApplication.class, args);
-	}
+		SpringApplication.run(IsemApplication.class, args);		
+	}	
+
 }
